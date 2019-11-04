51326d76dd9d5b7ca448e1c1d5f5c73961bbc9cd
Björn <me@tentakelfabrik.de>
2019-11-04 11:15:00 +0100

    adding

--- mrdoorbell.py
+++ mrdoorbell.py
@@ -1,13 +1,19 @@
 #
+# MrDoorbell
 #
+#
+# Installing:
+#
+# apt-get install rpi.gpio
 #
 # pip3 install discord.py
 # pip3 install python-dotenv
 #
+#
 
 import discord
-import re as regex
 import os
+import RPi.GPIO as GPIO
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -18,18 +24,33 @@ load_dotenv(find_dotenv())
 #
 class MrDoorbell(discord.Client):
 
-    #
-    #
-    #
-    #
-    async def on_ready(self):
-        await self._handleDoorbell()
+    EVERYONE = '@everyone'
 
     #
     #
     #
+    async def on_ready(self):
+        self._add_gpio()
+
     #
-    async def _handleDoorbell(self):
+    # adding pin for press button
+    #
+    #
+    def _add_gpio(self):
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+        GPIO.setup(os.getenv('GPIO_PIN'), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        GPIO.add_event_detect(os.getenv('GPIO_PIN'), GPIO.RISING, callback=self._handlePressDoorbell) # Setup event on pin 10 rising edge
+
+    #
+    # if doorbell is pressed create message and send to channel
+    #
+    #
+    async def _handlePressDoorbell(self):
+
+        GPIO.cleanup()
 
         # getting channel
         id = int(os.getenv('CHANNEL'))
@@ -39,19 +60,22 @@ class MrDoorbell(discord.Client):
         mentions = self._get_mentions(channel)
 
         # create content and send message, add image if MESSAGE_IMAGE is set
-        content = os.getenv('MESSAGE_TEXT') + mentions
+        content = os.getenv('MESSAGE_TEXT') + ' ' + mentions
         if (os.getenv('MESSAGE_IMAGE')):
             content += os.getenv('MESSAGE_IMAGE')
 
         await channel.send(content=content)
 
     #
+    # getting mentions for message
+    # if role isset, check if members of channel have role,
+    # if not mention @everyone
     #
-    #
+    # @return String
     #
     def _get_mentions(self, channel):
 
-        mentions = ' @everyone '
+        mentions = self.EVERYONE
 
         if (os.getenv('ROLE')):
 
@@ -59,8 +83,12 @@ class MrDoorbell(discord.Client):
 
             for member in channel.members:
                 for role in member.roles:
+
+                    # adding role
                     if role.name == os.getenv('ROLE'):
-                        mentions += ' ' + member.mention + ' '
+                        mentions += member.mention
+
+            mentions = mentions.strip()
 
         return mentions
 
